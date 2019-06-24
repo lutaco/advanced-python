39b9c2523be3bb950ebd8372d3ff92df27c4f1d7
ivan <molotkov.1997@mail.ru>
2019-06-24 12:13:33 +0300

    In the 'client' module, the ability to send data to the server is implemented

--- client/__main__.py
+++ client/__main__.py
@@ -1,8 +1,15 @@
 import yaml
+import json
+import socket
 import argparse
 
-from settings import ENCODING
+from settings import (
+    HOST, PORT, BUFFERSIZE, ENCODING,
+)
 
+host = HOST
+port = PORT
+buffersize = BUFFERSIZE
 encoding = ENCODING
 
 parser = argparse.ArgumentParser()
@@ -16,12 +23,28 @@ args = parser.parse_args()
 if args.config:
     with open(args.config) as file:
         conf = yaml.load(file, Loader=yaml.Loader)
+        host = conf.get('host', HOST)
+        port = conf.get('port', PORT)
+        buffersize = conf.get('buffersize', BUFFERSIZE)
         encoding = conf.get('encoding', ENCODING)
 
 try:
+    sock = socket.socket()
+    sock.connect((host, port))
     print('Client started')
-    value = input('enter data to sent: ')
-    bvalue = value.encode(encoding)
-    bvalue.decode()
+    data = input('enter data to sent: ')
+
+    request = json.dumps({
+        'data': data
+    })
+
+    sock.send(request.encode(encoding))
+    b_data = sock.recv(buffersize)
+    response = json.loads(
+        b_data.decode(encoding)
+    )
+    print(response)
+    sock.close()
+    
 except KeyboardInterrupt:
     print('Client closed')
